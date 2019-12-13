8fd77b1726b65e8c4d17cc9f3b110fc6f2104213
Ahmed Abdelfattah <ahmed.fattah98@eng-st.cu.edu.eg>
2019-12-13 15:48:10 +0200

    only excel sheet cells

--- preprocessing/libraries_mod.py
+++ preprocessing/libraries_mod.py
@@ -5,16 +5,16 @@ this module will contain all the libraries needed to be imported during the Proj
 
 """
 
-def mylibraries():
-    import numpy as np
-    import cv2
-    import skimage.io as io
-    from skimage.color import rgb2gray
-    from numba import vectorize, cuda
-    import matplotlib.pyplot as plt
-    from scipy import ndimage
-    from skimage.exposure import histogram
-    from skimage.measure import find_contours
-    from skimage.draw import rectangle, line
-    from skimage.transform import rotate
+from skimage.transform import hough_line, hough_line_peaks
+import numpy as np
+import cv2
+import skimage.io as io
+from skimage.color import rgb2gray
+from numba import vectorize, cuda
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from skimage.exposure import histogram
+from skimage.measure import find_contours
+from skimage.draw import rectangle, line
+from skimage.transform import rotate
 
