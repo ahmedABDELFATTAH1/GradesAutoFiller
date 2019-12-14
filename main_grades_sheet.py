19f002895d1d6fa591d4e583f395f3d9f86ccf55
Ahmed Abdelfattah <ahmed.fattah98@eng-st.cu.edu.eg>
2019-12-14 14:49:48 +0200

    deskew functionality added

--- main_grades_sheet.py
+++ main_grades_sheet.py
@@ -10,52 +10,12 @@ from skimage.exposure import histogram
 from skimage.measure import find_contours
 from skimage.draw import rectangle, line
 from skimage.transform import rotate
+from preprocessing.excellpre import *
 
-def returncell(row,col,Xlines,Ylines,binimage):
-    uppery=Ylines[col]
-    downy=Ylines[col+1]
-    leftx=Xlines[row]
-    rightx=Xlines[row+1]
-    return binimage[leftx:rightx,uppery:downy]
+rowscells=preprocessing('excelpic/8.jpg')
 
+cv2.imshow('img',rowscells[5][7])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-
-def detectHlines(binimage):
-    Xlines=[]   
-    onesmax=binimage.shape[1]
-    iprev=1000000
-    for i in range(binimage.shape[0]):
-        sliceimage=binimage[i,:]        
-        sumones=np.sum(sliceimage)        
-        if onesmax-sumones<5 and np.abs(i-iprev)>7:
-            Xlines.append(i)
-            iprev=i        
-    return Xlines
-
-def detectVlines(binimage):
-    ylines=[]
-    onesmax=binimage.shape[0]
-    iprev=1000000
-    for i in range(binimage.shape[1]):
-        sliceimage=binimage[:,i]        
-        sumones=np.sum(sliceimage )
-        if onesmax-sumones<5 and np.abs(i-iprev)>7:
-            ylines.append(i)
-            iprev=i
-    return ylines
-        
-
-
-# Read the image
-img = io.imread('excelimage.jpg')
-grayimag=rgb2gray(img)*255
-img_bin=grayimag<250
-img_bin=img_bin.astype(int)
-io.imshow(img_bin,cmap='gray')
-Xlines=detectHlines(img_bin)   
-Ylines=detectVlines(img_bin)  
- 
-nameimage=returncell(10,0,Xlines,Ylines,img)
-io.imshow(nameimage) 
-    
     
--- preprocessing/preprocessing_mod.py
+++ preprocessing/preprocessing_mod.py
@@ -7,14 +7,13 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 from skimage.exposure import histogram
 from skimage.measure import find_contours
-#from skimage.draw import rectangle, line
 from skimage.transform import rotate
 from skimage.filters import threshold_local,median
 from skimage.transform import hough_line, hough_line_peaks
 
 def intersection(line1, line2):  
-    rho1, theta1 = line1
-    rho2, theta2 = line2
+    rho1, theta1 = line1   
+    rho2, theta2 = line2   
     A = np.array([
         [np.cos(theta1), np.sin(theta1)],
         [np.cos(theta2), np.sin(theta2)]
@@ -24,8 +23,7 @@ def intersection(line1, line2):
     x0, y0 = int(np.round(x0)), int(np.round(y0))
     return [x0, y0]
 
-
-def preprocessing(path):
+def deskewImage(path):    
     colorimage= cv2.imread(path)
     gray_scale_image=cv2.cvtColor(colorimage,cv2.COLOR_BGR2GRAY)
     edge_gray_scale_image =cv2.Canny(gray_scale_image,100,150)
@@ -36,73 +34,94 @@ def preprocessing(path):
     img_rotation = cv2.warpAffine(gray_scale_image, rotation_matrix, (gray_scale_image.shape[1], gray_scale_image.shape[0]))
     colorimage=cv2.warpAffine(colorimage, rotation_matrix, (gray_scale_image.shape[1], gray_scale_image.shape[0]))
     edge_gray_scale_image =cv2.Canny(img_rotation,100,150)
-    th3 = cv2.adaptiveThreshold(img_rotation,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    
+    
+    cv2.imshow('img',gray_scale_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
     xlines=[]
     ylines=[]
-    lines=cv2.HoughLines(edge_gray_scale_image,1,np.pi/180,250)   
+    lines=cv2.HoughLines(edge_gray_scale_image,1,np.pi/180,200) 
+    
     for line in lines:    
-        rho,theta = line[0]       
+        rho,theta = line[0]  
         angle=((theta*180)/np.pi)+180
-        if (angle > 70 and angle < 120) or (angle> 240 and angle <300): 
+        if (angle > 80 and angle < 100) or (angle> 260 and angle <280): 
             xlines.append([rho,theta])             
-        elif (angle > 330) or(angle < 30) or (angle <210 and angle >150):
-            ylines.append([rho,theta])
-        filteredxlines=[]
-        xlines=sorted(xlines,key=lambda l:l[0])        
+        elif (angle > 320) or(angle < 20) or (angle <200 and angle >160):
+            ylines.append([rho,theta])      
+            
+    
+    xlines=sorted(xlines,key=lambda l:l[0])  
+    threshodlinex=5
+    while(1):        
+        filteredxlines=[]     
         prev_rho_x =100000
+        print(len(filteredxlines))
         for line in xlines:
-            rho=line[0]
-            print(rho)
-            if rho-prev_rho_x <10:
+            rho=line[0]        
+            if rho-prev_rho_x <threshodlinex:
                 prev_rho_x=rho
                 continue
             else:
                 filteredxlines.append(line)
                 prev_rho_x=rho
-    filteredylines=[]
-    ylines=sorted(ylines,key=lambda l:l[0])        
+        print(len(filteredxlines))            
+        if len(filteredxlines)>= 35 and  len(filteredxlines)<=42:
+            break
+        elif len(filteredxlines)<35:
+            threshodlinex-=1
+        else:
+            threshodlinex+=1
+            
+    ylines=sorted(ylines,key=lambda l:l[0])  
+    threshodliney=20
+      
+    filteredylines=[]     
     prev_rho_y =100000
     for line in ylines:
-        rho=line[0]
-        print(rho)
-        if np.abs(prev_rho_y-rho) <12:
+        rho=line[0]   
+        if np.abs(prev_rho_y-rho) <threshodliney:
             prev_rho_y=rho
             continue
         else:
             filteredylines.append(line)
             prev_rho_y=rho
-    rowscells = []
-    for i in range(len(filteredxlines)-1):
-        rowscells.append([])
-        linexup=filteredxlines[i]  
-        linexdown=filteredxlines[i+1]    
-        for j in range(1,len(filteredylines)):
-            lineyleft=filteredylines[j]
-            lineyright=filteredylines[j-1]
-            pointupleft=intersection(linexup,lineyleft)
-            pointdownleft=intersection(linexdown,lineyleft)
-            pointupright=intersection(linexup,lineyright)
-            pointdownright=intersection(linexdown,lineyright)         
-            width=int(np.sqrt(np.power(pointdownright[0]-pointdownleft[0],2)+np.power(pointdownright[1]-pointdownleft[1],2)))
-            height=int(np.sqrt(np.power(pointupleft[0]-pointdownleft[0],2)+np.power(pointupleft[1]-pointdownleft[1],2)))  
-            rect=np.array([
-                        pointupleft,pointupright,pointdownright,pointdownleft
-                        ],dtype = "float32")
-            dst = np.array([
-        		[0, 0],
-        		[width-1, 0],
-        		[width-1, height - 1],
-        		[0, height - 1]], dtype = "float32") 
-            	# compute the perspective transform matrix and then apply it
-            M=cv2.getPerspectiveTransform(rect, dst)
-            ped = cv2.warpPerspective(th3, M, (width, height))     
-            rowscells[i].append(ped)
-    return rowscells
-
-
-if __name__ =='__main__':
-    rowcells=preprocessing('excelpic/8.jpg')
+       
+          
     
+            
+    leftline=[10000,30]
+    rightline=filteredylines[0]
+    for line in filteredylines:
+        if(line[0]>0 and line[0]<leftline[0]):
+            leftline=line
     
-    
-
+    upline=filteredxlines[0]
+    downline=filteredxlines[len(filteredxlines)-1] 
+    pointupleft=intersection(upline,leftline)
+    pointdownleft=intersection(downline,leftline)
+    pointupright=intersection(upline,rightline)
+    pointdownright=intersection(downline,rightline)  
+    print(pointupleft)       
+    width=img_rotation.shape[1]
+    print(width)
+    height=img_rotation.shape[0]
+    rect=np.array([
+                pointupleft,pointupright,pointdownright,pointdownleft
+                ],dtype = "float32")
+    dst = np.array([
+    		[0, 0],
+    		[width-1, 0],
+    		[width-1, height - 1],
+    		[0, height - 1]], dtype = "float32") 
+    	# compute the perspective transform matrix and then apply it
+    M=cv2.getPerspectiveTransform(rect, dst)
+    deskewedimage = cv2.warpPerspective(img_rotation, M, (width, height)) 
+    return deskewedimage
+ 
+ 
+if __name__=='__main__':
+    deskewImage('the path you want')
+     
